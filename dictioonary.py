3aef364e88b5db652da931b36d56654f3de0b2e1
gonenshafry <gonenshafry@users.noreply.github.com>
2019-08-21 12:00:19 +0300

    Update dictioonary.py

--- dictioonary.py
+++ dictioonary.py
@@ -1,33 +1,33 @@
-import json;
-import difflib;
+import json
+import difflib
 
-data = json.load(open("data.json"));
+data = json.load(open("data.json"))
 
 def translate(w):
- w = w.lower();
+ w = w.lower()
 
  if w in data:
-  return data[w];
+  return data[w]
 
  elif len(difflib.get_close_matches(w,data.keys())) > 0:
   ww = input("Did you mean %s ?" % 
 difflib.get_close_matches(w, 
-data.keys())[0]);
+data.keys())[0])
   if ww.lower() == "y":
    return data[difflib.get_close_matches(w, 
 data.keys())[0]];
   elif ww.lower() == "n":
-   return "This word doesn't exist.";
+   return "This word doesn't exist."
 
  else:
-  return "This word doesn't exist.";
+  return "This word doesn't exist."
 
-word = input("Enter word: ");
+word = input("Enter word: ")
 
-output = translate(word);
+output = translate(word)
 
 if type(output) == list:
  for item in output:
-  print(item);
+  print(item)
 else:
- print(output);
+ print(output)
